85166f3695458e1bede46d732a375df619e72566
MLresearchLab <info@mlresearchlab.com>
2017-02-19 10:06:29 -0500

    Lab 1 Random Forest Code
    Lab 1 Random Forest Code
    - Updated Copyright Year

--- Lab1-Code.py
+++ Lab1-Code.py
@@ -7,7 +7,7 @@
 
 #MIT License
 
-#Copyright (c) 2016 Dickson Kwong & Kevin Figueroa
+#Copyright (c) 2017 Dickson Kwong & Kevin Figueroa
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
